4148f2b3dd51981e6f50a5ecf0c0612bda938982
Kha Dinh <Rawr@er-10-129-208-81.wireless.umn.edu>
2018-08-14 18:26:47 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # unit-6-GifTastic
+
+Some things to possibly add in the future:
+- [ ] Get random gifs
+- [ ] Align images so there is not so much white space
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -101,12 +101,13 @@ $("#add-person").on("click", function(event) {
     document.getElementById("person-input").value= "";
 });
 
-$(".gif").on("click", function() {
+$(document).on("click", ".gif", function() {
     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
     var state = $(this).attr("data-state");
     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
     // Then, set the image's data-state to animate
     // Else set src to the data-still value
+    console.log("clicked?");
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
